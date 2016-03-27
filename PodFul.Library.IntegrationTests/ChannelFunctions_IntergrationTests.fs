1e1b9cff22c0da2b93bb29c9229b4836810c8356
Benjamin Toynbee <toyners@hotmail.com>
2016-03-27 12:40:30 +1100

    Added F# integration test project.

---
+++ PodFul.Library.IntegrationTests/ChannelFunctions_IntergrationTests.fs
@@ -0,0 +1,13 @@
+﻿namespace PodFul.Library.IntegrationTests
+
+open FsUnit
+open NUnit.Framework
+open PodFul.Library
+
+type ChannelFunctions_IntergrationTests() = 
+
+    [<Test>]
+    member public this.``First Test``() = 
+        let x = 1
+
+        x |> should equal 1
