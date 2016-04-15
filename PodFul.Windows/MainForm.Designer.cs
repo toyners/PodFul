854aa8855c8d28eb6b88e5eb0164ed95406887f6
Benjamin Toynbee <toyners@hotmail.com>
2016-04-16 07:55:59 +1000

    Removed "Description" labels

--- PodFul.Windows/MainForm.Designer.cs
+++ PodFul.Windows/MainForm.Designer.cs
@@ -34,9 +34,7 @@
       this.addFeed = new System.Windows.Forms.Button();
       this.removeFeed = new System.Windows.Forms.Button();
       this.scanFeeds = new System.Windows.Forms.Button();
-      this.label2 = new System.Windows.Forms.Label();
       this.textBox1 = new System.Windows.Forms.TextBox();
-      this.label6 = new System.Windows.Forms.Label();
       this.textBox5 = new System.Windows.Forms.TextBox();
       this.label10 = new System.Windows.Forms.Label();
       this.SuspendLayout();
@@ -94,40 +92,22 @@
       this.scanFeeds.Text = "Scan";
       this.scanFeeds.UseVisualStyleBackColor = true;
       // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(9, 280);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(60, 13);
-      this.label2.TabIndex = 3;
-      this.label2.Text = "Description";
-      // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(12, 296);
+      this.textBox1.Location = new System.Drawing.Point(12, 282);
       this.textBox1.Multiline = true;
       this.textBox1.Name = "textBox1";
       this.textBox1.ReadOnly = true;
-      this.textBox1.Size = new System.Drawing.Size(223, 69);
+      this.textBox1.Size = new System.Drawing.Size(223, 83);
       this.textBox1.TabIndex = 4;
-      // 
-      // label6
-      // 
-      this.label6.AutoSize = true;
-      this.label6.Location = new System.Drawing.Point(257, 279);
-      this.label6.Name = "label6";
-      this.label6.Size = new System.Drawing.Size(60, 13);
-      this.label6.TabIndex = 3;
-      this.label6.Text = "Description";
       // 
       // textBox5
       // 
-      this.textBox5.Location = new System.Drawing.Point(260, 296);
+      this.textBox5.Location = new System.Drawing.Point(260, 282);
       this.textBox5.Multiline = true;
       this.textBox5.Name = "textBox5";
       this.textBox5.ReadOnly = true;
-      this.textBox5.Size = new System.Drawing.Size(223, 69);
+      this.textBox5.Size = new System.Drawing.Size(223, 83);
       this.textBox5.TabIndex = 4;
       // 
       // label10
@@ -146,8 +126,6 @@
       this.ClientSize = new System.Drawing.Size(497, 415);
       this.Controls.Add(this.textBox5);
       this.Controls.Add(this.textBox1);
-      this.Controls.Add(this.label6);
-      this.Controls.Add(this.label2);
       this.Controls.Add(this.scanFeeds);
       this.Controls.Add(this.removeFeed);
       this.Controls.Add(this.addFeed);
@@ -172,9 +150,7 @@
     private System.Windows.Forms.Button addFeed;
     private System.Windows.Forms.Button removeFeed;
     private System.Windows.Forms.Button scanFeeds;
-    private System.Windows.Forms.Label label2;
     private System.Windows.Forms.TextBox textBox1;
-    private System.Windows.Forms.Label label6;
     private System.Windows.Forms.TextBox textBox5;
     private System.Windows.Forms.Label label10;
   }
