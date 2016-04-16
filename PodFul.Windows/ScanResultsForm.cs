de6f6ade00e0f874b4347049377607c50a5c5cca
Benjamin Toynbee <toyners@hotmail.com>
2016-04-16 20:43:32 +1000

    Added scan results form.

---
+++ PodFul.Windows/ScanResultsForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PodFul.Windows
+{
+  public partial class ScanResultsForm : Form
+  {
+    public ScanResultsForm()
+    {
+      InitializeComponent();
+    }
+  }
+}
