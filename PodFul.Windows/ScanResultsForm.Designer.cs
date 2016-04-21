09f45e8c7fd78ae1e7647ed767d25da36d222da0
Benjamin Toynbee <toyners@hotmail.com>
2016-04-21 21:04:09 +1000

    Scan results grid is now populated.

--- PodFul.Windows/ScanResultsForm.Designer.cs
+++ PodFul.Windows/ScanResultsForm.Designer.cs
@@ -29,6 +29,11 @@
     private void InitializeComponent()
     {
       this.podcastList = new System.Windows.Forms.DataGridView();
+      this.startDownload = new System.Windows.Forms.Button();
+      this.titleColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.descriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.fileSizeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.latestDownloadDateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
       ((System.ComponentModel.ISupportInitialize)(this.podcastList)).BeginInit();
       this.SuspendLayout();
       // 
@@ -36,19 +41,75 @@
       // 
       this.podcastList.AllowUserToAddRows = false;
       this.podcastList.AllowUserToDeleteRows = false;
+      this.podcastList.AllowUserToResizeRows = false;
+      this.podcastList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
       this.podcastList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.podcastList.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.podcastList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.titleColumn,
+            this.descriptionColumn,
+            this.fileSizeColumn,
+            this.latestDownloadDateColumn});
+      this.podcastList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
       this.podcastList.Location = new System.Drawing.Point(0, 0);
       this.podcastList.Name = "podcastList";
       this.podcastList.ReadOnly = true;
-      this.podcastList.Size = new System.Drawing.Size(370, 258);
+      this.podcastList.RowHeadersVisible = false;
+      this.podcastList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+      this.podcastList.Size = new System.Drawing.Size(723, 258);
       this.podcastList.TabIndex = 0;
+      // 
+      // startDownload
+      // 
+      this.startDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.startDownload.Enabled = false;
+      this.startDownload.Location = new System.Drawing.Point(653, 267);
+      this.startDownload.Name = "startDownload";
+      this.startDownload.Size = new System.Drawing.Size(64, 35);
+      this.startDownload.TabIndex = 3;
+      this.startDownload.Text = "Start";
+      this.startDownload.UseVisualStyleBackColor = true;
+      // 
+      // titleColumn
+      // 
+      this.titleColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+      this.titleColumn.HeaderText = "Title";
+      this.titleColumn.Name = "titleColumn";
+      this.titleColumn.ReadOnly = true;
+      // 
+      // descriptionColumn
+      // 
+      this.descriptionColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+      this.descriptionColumn.FillWeight = 200F;
+      this.descriptionColumn.HeaderText = "Description";
+      this.descriptionColumn.Name = "descriptionColumn";
+      this.descriptionColumn.ReadOnly = true;
+      // 
+      // fileSizeColumn
+      // 
+      this.fileSizeColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+      this.fileSizeColumn.FillWeight = 40F;
+      this.fileSizeColumn.HeaderText = "File Size";
+      this.fileSizeColumn.Name = "fileSizeColumn";
+      this.fileSizeColumn.ReadOnly = true;
+      this.fileSizeColumn.Width = 80;
+      // 
+      // latestDownloadDateColumn
+      // 
+      this.latestDownloadDateColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+      this.latestDownloadDateColumn.FillWeight = 60F;
+      this.latestDownloadDateColumn.HeaderText = "Last Download Date";
+      this.latestDownloadDateColumn.Name = "latestDownloadDateColumn";
+      this.latestDownloadDateColumn.ReadOnly = true;
+      this.latestDownloadDateColumn.Width = 140;
       // 
       // ScanResultsForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(370, 258);
+      this.ClientSize = new System.Drawing.Size(724, 309);
+      this.Controls.Add(this.startDownload);
       this.Controls.Add(this.podcastList);
       this.MinimizeBox = false;
       this.Name = "ScanResultsForm";
@@ -61,5 +122,10 @@
     #endregion
 
     private System.Windows.Forms.DataGridView podcastList;
+    private System.Windows.Forms.Button startDownload;
+    private System.Windows.Forms.DataGridViewTextBoxColumn titleColumn;
+    private System.Windows.Forms.DataGridViewTextBoxColumn descriptionColumn;
+    private System.Windows.Forms.DataGridViewTextBoxColumn fileSizeColumn;
+    private System.Windows.Forms.DataGridViewTextBoxColumn latestDownloadDateColumn;
   }
 }
--- PodFul.Windows/ScanResultsForm.cs
+++ PodFul.Windows/ScanResultsForm.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace PodFul.Windows
 {
+  using System;
   using System.Collections.Generic;
   using System.Windows.Forms;
   using PodFul.Library;
@@ -13,7 +14,11 @@ namespace PodFul.Windows
 
       foreach (var podcast in podcasts)
       {
-        this.podcastList.Rows.Add(podcast.Title, podcast.Description, podcast.FileSize);
+        this.podcastList.Rows.Add(
+          podcast.Title, 
+          podcast.Description, 
+          podcast.FileSize, 
+          podcast.LatestDownloadDate != DateTime.MinValue ? podcast.LatestDownloadDate.ToString("ddd, dd-MM-yyyy") : @"n\a");
       }
     }
   }
