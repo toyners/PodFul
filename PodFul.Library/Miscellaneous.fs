c57e18d282e9b010e07dc4fa14bc73380800ac4a
Benjamin Toynbee <toyners@hotmail.com>
2016-04-25 17:37:26 +1000

    Added function to return raw size (in bytes) as a string (in Mb).

---
+++ PodFul.Library/Miscellaneous.fs
@@ -0,0 +1,11 @@
+﻿namespace PodFul.Library
+
+open System
+
+module public Miscellaneous = 
+
+    let public GetReadableFileSize (rawSize : Double) : string = 
+        
+        let sizeInMb = rawSize / (1048576.0)
+
+        sizeInMb.ToString("00.0")
