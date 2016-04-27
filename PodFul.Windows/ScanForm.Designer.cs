6d27a4b44d7562237ae56bd8a31b2202b9a1e866
Benjamin Toynbee <toyners@hotmail.com>
2016-04-27 19:15:02 +1000

    Completed initial scanning functionality. Runs as asynchronous operation.

--- PodFul.Windows/ScanForm.Designer.cs
+++ PodFul.Windows/ScanForm.Designer.cs
@@ -28,117 +28,52 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.podcastList = new System.Windows.Forms.DataGridView();
-      this.titleColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-      this.descriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-      this.fileSizeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-      this.latestDownloadDateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
       this.cancelButton = new System.Windows.Forms.Button();
-      this.startDownload = new System.Windows.Forms.Button();
-      ((System.ComponentModel.ISupportInitialize)(this.podcastList)).BeginInit();
+      this.feedback = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
-      // 
-      // podcastList
-      // 
-      this.podcastList.AllowUserToAddRows = false;
-      this.podcastList.AllowUserToDeleteRows = false;
-      this.podcastList.AllowUserToResizeRows = false;
-      this.podcastList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-      this.podcastList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.podcastList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.titleColumn,
-            this.descriptionColumn,
-            this.fileSizeColumn,
-            this.latestDownloadDateColumn});
-      this.podcastList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-      this.podcastList.Location = new System.Drawing.Point(0, 0);
-      this.podcastList.Name = "podcastList";
-      this.podcastList.ReadOnly = true;
-      this.podcastList.RowHeadersVisible = false;
-      this.podcastList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-      this.podcastList.Size = new System.Drawing.Size(723, 258);
-      this.podcastList.TabIndex = 1;
-      // 
-      // titleColumn
-      // 
-      this.titleColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-      this.titleColumn.HeaderText = "Title";
-      this.titleColumn.Name = "titleColumn";
-      this.titleColumn.ReadOnly = true;
-      // 
-      // descriptionColumn
-      // 
-      this.descriptionColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-      this.descriptionColumn.FillWeight = 200F;
-      this.descriptionColumn.HeaderText = "Description";
-      this.descriptionColumn.Name = "descriptionColumn";
-      this.descriptionColumn.ReadOnly = true;
-      // 
-      // fileSizeColumn
-      // 
-      this.fileSizeColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-      this.fileSizeColumn.FillWeight = 40F;
-      this.fileSizeColumn.HeaderText = "File Size";
-      this.fileSizeColumn.Name = "fileSizeColumn";
-      this.fileSizeColumn.ReadOnly = true;
-      this.fileSizeColumn.Width = 80;
-      // 
-      // latestDownloadDateColumn
-      // 
-      this.latestDownloadDateColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-      this.latestDownloadDateColumn.FillWeight = 60F;
-      this.latestDownloadDateColumn.HeaderText = "Last Download Date";
-      this.latestDownloadDateColumn.Name = "latestDownloadDateColumn";
-      this.latestDownloadDateColumn.ReadOnly = true;
-      this.latestDownloadDateColumn.Width = 140;
       // 
       // cancelButton
       // 
       this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.cancelButton.Location = new System.Drawing.Point(583, 267);
+      this.cancelButton.Location = new System.Drawing.Point(411, 267);
       this.cancelButton.Name = "cancelButton";
       this.cancelButton.Size = new System.Drawing.Size(64, 35);
       this.cancelButton.TabIndex = 7;
       this.cancelButton.Text = "Cancel";
       this.cancelButton.UseVisualStyleBackColor = true;
+      this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
       // 
-      // startDownload
+      // feedback
       // 
-      this.startDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.startDownload.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.startDownload.Location = new System.Drawing.Point(653, 267);
-      this.startDownload.Name = "startDownload";
-      this.startDownload.Size = new System.Drawing.Size(64, 35);
-      this.startDownload.TabIndex = 6;
-      this.startDownload.Text = "Start";
-      this.startDownload.UseVisualStyleBackColor = true;
+      this.feedback.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.feedback.Location = new System.Drawing.Point(10, 9);
+      this.feedback.Multiline = true;
+      this.feedback.Name = "feedback";
+      this.feedback.ReadOnly = true;
+      this.feedback.Size = new System.Drawing.Size(465, 252);
+      this.feedback.TabIndex = 8;
       // 
-      // ScanResultsForm
+      // ScanForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(724, 309);
+      this.CancelButton = this.cancelButton;
+      this.ClientSize = new System.Drawing.Size(481, 307);
+      this.Controls.Add(this.feedback);
       this.Controls.Add(this.cancelButton);
-      this.Controls.Add(this.startDownload);
-      this.Controls.Add(this.podcastList);
-      this.Name = "ScanResultsForm";
+      this.MinimizeBox = false;
+      this.Name = "ScanForm";
       this.Text = "ScanResultsForm";
-      ((System.ComponentModel.ISupportInitialize)(this.podcastList)).EndInit();
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
-
-    private System.Windows.Forms.DataGridView podcastList;
-    private System.Windows.Forms.DataGridViewTextBoxColumn titleColumn;
-    private System.Windows.Forms.DataGridViewTextBoxColumn descriptionColumn;
-    private System.Windows.Forms.DataGridViewTextBoxColumn fileSizeColumn;
-    private System.Windows.Forms.DataGridViewTextBoxColumn latestDownloadDateColumn;
     private System.Windows.Forms.Button cancelButton;
-    private System.Windows.Forms.Button startDownload;
+    private System.Windows.Forms.TextBox feedback;
   }
 }
--- PodFul.Windows/ScanForm.cs
+++ PodFul.Windows/ScanForm.cs
@@ -3,33 +3,82 @@ namespace PodFul.Windows
 {
   using System;
   using System.Collections.Generic;
+  using System.Threading;
+  using System.Threading.Tasks;
   using System.Windows.Forms;
   using PodFul.Library;
 
   public partial class ScanForm : Form
   {
-    public ScanForm(IEnumerable<Feed> feeds)
+    private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+    private TaskScheduler mainTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+    public ScanForm(IList<Feed> feeds)
     {
       InitializeComponent();
+      this.Text = "Scanning " + feeds.Count + " feed" + (feeds.Count != 1 ? "s" : String.Empty);
 
-      List<Podcast> newPodcasts = new List<Podcast>();
-      foreach (var feed in feeds)
+      CancellationToken cancellationToken = this.cancellationTokenSource.Token;
+
+      Task task = Task.Factory.StartNew(() =>
       {
-        var podcasts = FeedFunctions.CreatePodcastList(feed.URL);
-
-        Int32 index = 0;
-        while (index < podcasts.Length && index < feed.Podcasts.Length && !podcasts[index].Equals(feed.Podcasts[index]))
+        Dictionary<Feed, List<Podcast>> updatedFeeds = new Dictionary<Feed, List<Podcast>>();
+        String scanReport = null;
+        foreach (var feed in feeds)
         {
-          newPodcasts.Add(podcasts[index]);
-          index++;
-        }
-      }
+          if (this.cancellationTokenSource.IsCancellationRequested)
+          {
+            this.PostMessage("\r\nCANCELLED");
+            return;
+          }
 
-      if (newPodcasts.Count == 0)
+          this.PostMessage("Scanning \"" + feed.Title + "\".");
+
+          var newFeed = FeedFunctions.CreateFeed(feed.URL, String.Empty);
+
+          this.PostMessage("Podcast list read.");
+
+          Int32 index = 0;
+          List<Podcast> newPodcasts = new List<Podcast>();
+          while (index < newFeed.Podcasts.Length && index < feed.Podcasts.Length && !newFeed.Podcasts[index].Equals(feed.Podcasts[index]))
+          {
+            newPodcasts.Add(newFeed.Podcasts[index]);
+            index++;
+          }
+
+          String message = "Scan completed. ";
+          if (index == 0)
+          {
+            message += "No new podcasts found.";
+          }
+          else
+          {
+            scanReport += String.Format("\"{0}\": {1} podcast{2} found.\r\n", feed.Title, index, (index != 1 ? "s" : String.Empty));
+            message += index + " new podcast" + (index != 1 ? "s" : String.Empty) + " found.";
+            updatedFeeds.Add(newFeed, newPodcasts);
+          }
+
+          this.PostMessage(message + "\r\n");
+        }
+
+        if (scanReport != null)
+        {
+          this.PostMessage("Scan Report\r\n" + scanReport);
+        }
+      }, cancellationToken);
+    }
+
+    private void PostMessage(String message)
+    {
+      new Task(() =>
       {
-        MessageBox.Show("No new podcasts found", "Podful - Scan results", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-        return;
-      }
+        this.feedback.Text += message + "\r\n";
+      }).Start(this.mainTaskScheduler);
+    }
+
+    private void cancelButton_Click(Object sender, EventArgs e)
+    {
+      this.cancellationTokenSource.Cancel();
     }
   }
 }
