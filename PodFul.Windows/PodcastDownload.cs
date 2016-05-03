9ffbca2579631f6972ad7432710e59a2f5f2b9e9
toyners <toyners@hotmail.com>
2016-05-03 21:10:40 +1000

    Completed first version of PodcastDownload class.

--- PodFul.Windows/PodcastDownload.cs
+++ PodFul.Windows/PodcastDownload.cs
@@ -6,35 +6,96 @@ namespace PodFul.Windows
   using System.IO;
   using System.Linq;
   using System.Text;
+  using System.Threading;
   using System.Threading.Tasks;
+  using Jabberwocky.Toolkit.Object;
   using Library;
 
   public delegate void ResetProgressDelegate(Int64 fileSize);
   public delegate void PostMessageDelegate(String message);
   public delegate void PostMessageWithLineBreakDelegate(String message, Boolean addLineBreak);
+  public delegate void DownloadCompleteDelegate(Podcast podcast);
+  public delegate void DownloadSuccessfulDelegate(Podcast podcast);
 
-  public static class PodcastDownload
+  public class PodcastDownload
   {
-    public static event ResetProgressDelegate ResetProgress;
+    private CancellationToken cancellationToken;
+
+    private Action<Int32> updateProgress;
+
+    public PodcastDownload(CancellationToken cancellationToken, Action<Int32> updateProgress)
+    {
+      cancellationToken.VerifyThatObjectIsNotNull("Parameter 'cancellationToken' is null.");
+      if (updateProgress == null)
+      {
+        throw new Exception("Parameter 'úpdateProgress' is null.");
+      }
+
+      this.cancellationToken = cancellationToken;
+      this.updateProgress = updateProgress;
+    }
+
+    public event ResetProgressDelegate ResetProgress;
 
     public static event PostMessageDelegate PostMessage;
 
-    public static event PostMessageWithLineBreakDelegate PostMessageWithLineBreak;
+    public event PostMessageWithLineBreakDelegate PostMessageWithLineBreak;
 
-    public static Boolean Download(String directoryPath, Podcast[] podcasts, Queue<Int32> selectedPodcasts)
+    public event DownloadCompleteDelegate DownloadComplete;
+
+    public event DownloadSuccessfulDelegate DownloadSuccessful;
+
+    public Boolean Download(String directoryPath, Podcast[] podcasts, Queue<Int32> podcastsIndexes)
     {
       BigFileDownloader downloader = new BigFileDownloader();
 
-      var podcast = podcasts[0];
+      while (podcastsIndexes.Count > 0)
+      {
+        var podcastIndex = podcastsIndexes.Dequeue();
+        var podcast = podcasts[podcastIndex];
 
-      ResetProgress?.Invoke(podcast.FileSize);
-      PostMessageWithLineBreak?.Invoke(String.Format("Downloading \"{0}\" ...", podcast.Title), false);
+        this.ResetProgress?.Invoke(podcast.FileSize);
+        this.PostMessageWithLineBreak?.Invoke(String.Format("Downloading \"{0}\" ...", podcast.Title), false);
 
-      var filePath = Path.Combine(directoryPath, podcast.URL.Substring(podcast.URL.LastIndexOf('/') + 1));
-      Task downloadTask = downloader.DownloadAsync(podcast.URL, filePath, this.cancellationToken, this.UpdateProgessEventHandler);
+        var filePath = Path.Combine(directoryPath, podcast.URL.Substring(podcast.URL.LastIndexOf('/') + 1));
+        Task downloadTask = downloader.DownloadAsync(podcast.URL, filePath, this.cancellationToken, this.updateProgress);
 
+        Boolean downloadSuccessful = true;
+        try
+        {
+          downloadTask.Wait();
+        }
+        catch (AggregateException exception)
+        {
+          downloadSuccessful = false;
+          Exception e = exception.Flatten();
+          if (e.InnerException != null)
+          {
+            e = e.InnerException;
+          }
 
-      throw new NotImplementedException();
+          this.PostMessageWithLineBreak?.Invoke(e.Message, false);
+        }
+
+        this.DownloadComplete?.Invoke(podcast);
+
+        if (downloadTask.IsCanceled)
+        {
+          // Downloading cancelled. Regardless of what was previously downloaded we will not
+          // update the feed file.
+          return false;
+        }
+
+        if (downloadSuccessful && this.DownloadSuccessful != null)
+        {
+          this.DownloadSuccessful(podcast);
+        }
+
+        podcasts[podcastIndex] = Podcast.SetDownloadDate(podcast, DateTime.Now);
+      }
+
+      this.ResetProgress?.Invoke(0);
+      return true;
     }
   }
 }
