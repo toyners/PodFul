3da4ecc79890cfadffbafa4c528e88cd73907fba
toyners <toyners@hotmail.com>
2016-05-20 21:25:37 +1000

    Added Implementation of IFeedStorage - Feeds stored using files - and expanded on IFeedStorage

---
+++ PodFul.Library/FeedFileStorage.fs
@@ -0,0 +1,48 @@
+﻿namespace PodFul.Library
+
+open System
+open System.Collections.Generic
+open System.IO
+
+type FeedFileStorage(directoryPath : String) = 
+
+    let mutable isopen = false
+    let feeds = new Dictionary<Feed, String>()
+    let mutable feedKeys = [||]
+
+    interface IFeedStorage with
+
+        member this.Feeds with get() = 
+                            match isopen with
+                            | true -> feedKeys
+                            | _ -> failwith "FileFeedStorage is not open"
+
+        member this.IsOpen with get() = isopen
+
+        member this.Add (feed : Feed) =
+            let filePath = ""
+            FeedFunctions.WriteFeedToFile feed filePath
+            feeds.Add(feed, filePath)
+            feeds.Keys.CopyTo(feedKeys, feeds.Keys.Count)
+
+        member this.Close() =
+            feeds.Clear()
+            isopen <- false
+
+        member this.Open () = 
+            let filePaths = Directory.GetFiles(directoryPath, "*.txt", SearchOption.TopDirectoryOnly)
+            for filePath in filePaths do
+                feeds.Add(FeedFunctions.ReadFeedFromFile(filePath), filePath)
+            isopen <- true
+
+        member this.Remove (feed : Feed) = 
+            let filePath = feeds.[feed]
+            File.Delete(filePath)
+            feeds.Remove(feed) |> ignore
+            
+        member this.Update (feed : Feed) =
+            let filePath = feeds.[feed]
+            FeedFunctions.WriteFeedToFile feed filePath |> ignore
+            
+    member public this.Storage () =
+            this :> IFeedStorage
--- PodFul.Library/IFeedStorage.fs
+++ PodFul.Library/IFeedStorage.fs
@@ -4,10 +4,17 @@ open System
 
 type IFeedStorage =
 
-    abstract member Feeds : Feed[]
+    abstract member Feeds : Feed[] with get
 
-    abstract member IsOpen : Boolean
+    abstract member IsOpen : Boolean with get
+
+    abstract member Add : Feed -> unit
+
+    abstract member Close : unit -> unit
 
     abstract member Open : unit -> unit
 
+    abstract member Remove : Feed -> unit
+
     abstract member Update : Feed -> unit
+
