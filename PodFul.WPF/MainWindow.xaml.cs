31a1d99d52dd89039fec23dd5622b640def72660
toyners <toyners@hotmail.com>
2016-05-23 19:13:24 +1000

    Now setting title of window in WPF project.

--- PodFul.WPF/MainWindow.xaml.cs
+++ PodFul.WPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PodFul.Library;
 
 namespace PodFul.WPF
 {
@@ -23,6 +25,21 @@ namespace PodFul.WPF
     public MainWindow()
     {
       InitializeComponent();
+
+      this.DisplayTitle();
+    }
+
+    private void DisplayTitle()
+    {
+      var guiVersion = Assembly.GetExecutingAssembly().GetName().Version;
+      var libraryVersion = Assembly.GetAssembly(typeof(IFeedStorage)).GetName().Version;
+      this.Title = String.Format("PodFul - v{0}.{1}.{2} (v{3}.{4}.{5})",
+        guiVersion.Major,
+        guiVersion.Minor,
+        guiVersion.Build,
+        libraryVersion.Major,
+        libraryVersion.Minor,
+        libraryVersion.Build);
     }
   }
 }
