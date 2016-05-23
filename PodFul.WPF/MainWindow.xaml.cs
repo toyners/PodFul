71bc08126b426cf283364aa57930cf2f7c10266b
toyners <toyners@hotmail.com>
2016-05-24 08:39:29 +1000

    Added basic Feed list populated from feed storage.

--- PodFul.WPF/MainWindow.xaml.cs
+++ PodFul.WPF/MainWindow.xaml.cs
@@ -1,32 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using PodFul.Library;
-
+﻿
 namespace PodFul.WPF
 {
+  using System;
+  using System.Collections.Generic;
+  using System.Configuration;
+  using System.Linq;
+  using System.Reflection;
+  using System.Text;
+  using System.Threading.Tasks;
+  using System.Windows;
+  using System.Windows.Controls;
+  using System.Windows.Data;
+  using System.Windows.Documents;
+  using System.Windows.Input;
+  using System.Windows.Media;
+  using System.Windows.Media.Imaging;
+  using System.Windows.Navigation;
+  using System.Windows.Shapes;
+  using PodFul.Library;
+
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
   public partial class MainWindow : Window
   {
+    private IFeedStorage feedStorage;
+    private Feed currentFeed;
+     
     public MainWindow()
     {
       InitializeComponent();
 
       this.DisplayTitle();
+
+      var feedDirectory = ConfigurationManager.AppSettings["FeedDirectory"];
+      this.feedStorage = new FeedFileStorage(feedDirectory);
+      this.feedStorage.Open();
+      
+      FeedList.ItemsSource = this.feedStorage.Feeds;
     }
 
     private void DisplayTitle()
