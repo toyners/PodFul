fa854dfd816696e18c3510bacfdec5699dd60bc7
toyners <toyners@hotmail.com>
2016-05-25 08:08:50 +1000

    Refactoring only. Removed unneeded using statement.

--- PodFul.Winforms/ProcessingForm.cs
+++ PodFul.Winforms/ProcessingForm.cs
@@ -4,7 +4,6 @@ namespace PodFul.Winforms
   using System;
   using System.Collections.Generic;
   using System.Diagnostics;
-  using System.IO;
   using System.Threading;
   using System.Threading.Tasks;
   using System.Windows.Forms;
