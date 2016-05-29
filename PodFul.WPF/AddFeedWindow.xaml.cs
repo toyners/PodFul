79edd304168aabb979b21c79a4c3291f65283a03
toyners <toyners@hotmail.com>
2016-05-30 08:33:03 +1000

    Completed UI logic for add feed window.

--- PodFul.WPF/AddFeedWindow.xaml.cs
+++ PodFul.WPF/AddFeedWindow.xaml.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
+﻿
 namespace PodFul.WPF
 {
+  using System;
+  using System.Collections.Generic;
+  using System.IO;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  using System.Windows;
+  using System.Windows.Controls;
+  using System.Windows.Data;
+  using System.Windows.Documents;
+  using System.Windows.Input;
+  using System.Windows.Media;
+  using System.Windows.Media.Imaging;
+  using System.Windows.Shapes;
+
   /// <summary>
   /// Interaction logic for AddFeedWindow.xaml
   /// </summary>
@@ -26,7 +28,35 @@ namespace PodFul.WPF
 
     private void okButton_Click(Object sender, RoutedEventArgs e)
     {
+      var directoryPath = this.DirectoryPath.Text;
+      if (!directoryPath.EndsWith(@"\", StringComparison.Ordinal))
+      {
+        directoryPath += @"\";
+      }
 
+      if (!Directory.Exists(directoryPath))
+      {
+        var message = String.Format("Directory '{0}' does not exist. Create it now?", directoryPath);
+        var createDirectory = MessageBox.Show(message, "Directory does not exist", MessageBoxButton.YesNoCancel, MessageBoxImage.Question, MessageBoxResult.Yes);
+
+        if (createDirectory == MessageBoxResult.Cancel)
+        {
+          return;
+        }
+
+        if (createDirectory == MessageBoxResult.Yes)
+        {
+          Directory.CreateDirectory(directoryPath);
+        }
+      }
+
+      this.DialogResult = true;
+    }
+
+    private void textChanged(Object sender, TextChangedEventArgs e)
+    {
+      this.OkButton.IsEnabled = (this.DirectoryPath.Text.Length > 0 && this.URL.Text.Length > 0);
+      e.Handled = true;
     }
   }
 }
