721d80f7c7813e9a9844c89937ed8a15a321c777
toyners <toyners@hotmail.com>
2016-06-02 19:28:46 +1000

    Set text wrapping on processing window. Moved over progress handler and supportinf fields/code.

--- PodFul.WPF/ProcessingWindow.xaml.cs
+++ PodFul.WPF/ProcessingWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,13 @@ namespace PodFul.WPF
   /// </summary>
   public partial class ProcessingWindow : Window
   {
+    private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+    private TaskScheduler mainTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+    private Int64 fileSize;
+    private Int64 downloadedSize;
+    private Int64 percentageStepSize;
+    private Boolean fileSizeNotKnown;
+
     public ProcessingWindow(IFeedStorage feedStorage, Queue<Int32> feedIndexes)
     {
       InitializeComponent();
@@ -33,6 +41,39 @@ namespace PodFul.WPF
     private void Cancel_Click(Object sender, RoutedEventArgs e)
     {
 
+    }
+
+    private void UpdateProgessEventHandler(Int32 bytesWrittenToFile)
+    {
+      if (this.fileSizeNotKnown)
+      {
+        return;
+      }
+
+      new Task(() =>
+      {
+        this.downloadedSize += bytesWrittenToFile;
+        if (this.downloadedSize > this.fileSize)
+        {
+          this.Progress.Value = 100;
+          return;
+        }
+
+        Int64 steps = this.downloadedSize / this.percentageStepSize;
+        if (steps > this.Progress.Value)
+        {
+          this.Progress.Value = (Int32)steps;
+        }
+      }).Start(this.mainTaskScheduler);
+    }
+
+    private void ResetProgressBar(Int64 expectedFileSize = -1)
+    {
+      new Task(() =>
+      {
+        this.Progress.Value = 0;
+        this.Progress.IsIndeterminate = (expectedFileSize == 0);
+      }).Start(this.mainTaskScheduler);
     }
   }
 }
