517e7ba2bbc134ff213f5b426e1976b2cdf4d1f8
toyners <toyners@hotmail.com>
2016-06-10 18:09:14 +1000

    Completed implementation of ImageResolver.GetName

--- PodFul.Library/ImageResolver.fs
+++ PodFul.Library/ImageResolver.fs
@@ -1,10 +1,36 @@
 ﻿namespace PodFul.Library
 
+open System
+open System.IO
+open System.Collections.Generic
+open Jabberwocky.Toolkit.String
+
 type ImageResolver(imageDirectoryPath : string) =
 
     let directoryPath = imageDirectoryPath
 
+    member private this.fileNameSubstitutions = Dictionary<String, String>(dict
+                                                    [
+                                                        ( "\\", "_bs_" );
+                                                        ( "/", "_fs_" );
+                                                        ( ":", "_c_" );
+                                                        ( "*", "_a_" );
+                                                        ( "?", "_q_" );
+                                                        ( "\"", "_qu_" );
+                                                        ( "<", "_l_" );
+                                                        ( ">", "_g_" );
+                                                        ( "|", "_b_" )
+                                                    ])
+
     interface IImageResolver with
         
         member this.GetName (imageFileName : string) : string = 
-            ""
+
+            let cleanImageFileName = imageFileName.Substitute(this.fileNameSubstitutions);
+            let imageFilePath = Path.Combine(directoryPath, cleanImageFileName)
+
+            if File.Exists(imageFilePath) = false then
+                let imageDownloader = new BigFileDownloader()
+                imageDownloader.DownloadAsync(imageFileName, imageFilePath, Threading.CancellationToken.None, null) |> ignore              
+
+            imageFilePath
