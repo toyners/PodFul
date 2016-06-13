e215f8af624df80e7737c6b78536917994ea2870
Benjamin Toynbee <toyners@hotmail.com>
2016-06-13 16:45:36 +1000

    Added converter to convert the feed creation date times to user friendly strings for use in the feed list. Added unit testing project for testing converter.

---
+++ PodFul.WPF.UnitTests/CreationDateTimeConverter_UnitTests.fs
@@ -0,0 +1,58 @@
+﻿namespace PodFul.WPF.UnitTests
+
+open System
+open FsUnit
+open NUnit.Framework
+open PodFul.WPF
+
+/// <summary>
+/// Tests the different values that the datetime to string converter can be passed. Creates user friendly strings. 
+/// </summary>
+type CreationDateTimeConverter_UnitTests() = 
+     
+     [<Test>]
+     member public this.``Value is not of datetime type so empty string is returned.``() =
+        let converter = new CreationDateTimeConverter()
+        converter.Convert(new Object(), null, null, null) |> should equal String.Empty;
+
+     [<Test>]
+     member public this.``Value is DateTime.MinValue so empty string is returned.``() =
+        let converter = new CreationDateTimeConverter()
+        converter.Convert(new Object(), null, null, null) |> should equal String.Empty;
+
+     [<Test>]
+     member public this.``Value is an hour ago so string starts with Today.``() =
+        let converter = new CreationDateTimeConverter()
+        let value = DateTime.Now - TimeSpan.FromHours(1.0)
+        let resultTime = value.ToString("HH:mm:ss");
+        converter.Convert(value, null, null, null) |> should equal ("Last Updated: Today at " + resultTime)
+
+     [<Test>]
+     member public this.``Value is a day ago so string starts with Yesterday.``() =
+        let converter = new CreationDateTimeConverter()
+        let value = DateTime.Now - TimeSpan.FromDays(1.0)
+        let resultTime = value.ToString("HH:mm:ss");
+        converter.Convert(value, null, null, null) |> should equal ("Last Updated: Yesterday at " + resultTime)
+
+     [<Test>]
+     [<TestCase(2)>]
+     [<TestCase(3)>]
+     [<TestCase(4)>]
+     [<TestCase(5)>]
+     [<TestCase(6)>]
+     [<TestCase(7)>]
+     member public this.``Value is up to a week ago so string starts with Day name.``(dayCount : float) =
+        let converter = new CreationDateTimeConverter()
+        let value = DateTime.Now - TimeSpan.FromDays(dayCount)
+        let dayName = value.ToString("dddd")
+        let resultTime = value.ToString("HH:mm:ss");
+        converter.Convert(value, null, null, null) |> should equal ("Last Updated: " + dayName + " at " + resultTime)
+
+     [<Test>]
+     [<TestCase(8)>]
+     [<TestCase(999)>]
+     member public this.``Value is over a week ago so string starts with Date.``(dayCount : float) =
+        let converter = new CreationDateTimeConverter()
+        let value = DateTime.Now - TimeSpan.FromDays(dayCount)
+        let resultDateTime = value.ToString("dd-MMM-yyyy HH:mm:ss");
+        converter.Convert(value, null, null, null) |> should equal ("Last Updated: " + resultDateTime)
---
+++ PodFul.WPF/CreationDateTimeConverter.cs
@@ -0,0 +1,54 @@
+﻿
+namespace PodFul.WPF
+{
+  using System;
+  using System.Globalization;
+  using System.Windows;
+  using System.Windows.Data;
+
+  /// <summary>
+  /// Converts datetime to user friendly string for use with the feed creation time.
+  /// </summary>
+  [ValueConversion(typeof(DateTime), typeof(String))]
+  public class CreationDateTimeConverter : IValueConverter
+  {
+    public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+    {
+      if (!(value is DateTime))
+      {
+        return String.Empty;
+      }
+
+      var creationDateTime = (DateTime)value;
+
+      if (creationDateTime == DateTime.MinValue)
+      {
+        return String.Empty;
+      }
+
+      var timeSpan = DateTime.Now - creationDateTime;
+
+      if (timeSpan.Days == 0)
+      {
+        return String.Format("Last Updated: Today at {0:HH:mm:ss}", creationDateTime);
+      }
+
+      if (timeSpan.Days == 1)
+      {
+        return String.Format("Last Updated: Yesterday at {0:HH:mm:ss}", creationDateTime);
+      }
+
+      if (timeSpan.Days < 8)
+      {
+        return String.Format("Last Updated: {0:dddd} at {0:HH:mm:ss}", creationDateTime);
+      }
+
+      return String.Format("Last Updated: {0:dd-MMM-yyyy HH:mm:ss}", creationDateTime);
+    }
+
+    public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+    {
+      return DependencyProperty.UnsetValue;
+    }
+  }
+}
