e48fa0fcecada9b693987e11d75aab9d1d90e73a
toyners <toyners@hotmail.com>
2016-06-15 09:04:40 +1000

    Added new PodcastDownloader type file. One method only. Meant to replace PodcastDownload class from UI layer.

---
+++ PodFul.Library/PodcastDownloader.fs
@@ -0,0 +1,11 @@
+﻿namespace PodFul.Library
+
+open System
+
+type PodcastDownloader() =
+
+    //member public this.OnSuccessfulDownload : Action<Podcast, String> with get, set
+
+    member public this.Download directoryPath podcasts podcastIndexes = 
+        let downloader = new FileDownloader()
+        ignore
