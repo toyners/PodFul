536ccc28bebc4b289ea148115b09d1a571339d54
Benjamin Toynbee <toyners@hotmail.com>
2016-06-26 08:04:01 +1000

    Small refactoring: removed unused librarys

--- PodFul.WPF/FeedScanner.cs
+++ PodFul.WPF/FeedScanner.cs
@@ -3,8 +3,6 @@ namespace PodFul.WPF
 {
   using System;
   using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
   using System.Threading;
   using System.Threading.Tasks;
   using System.Windows;
