00e688635b6ee6a0c8b9a1dc6060653e4d294b1f
toyners <toyners@hotmail.com>
2016-06-27 18:17:58 +1000

    Added converter class that truncates the feed description to less than 100 complete words. Included unit tests for converter.

---
+++ PodFul.WPF.UnitTests/TextTruncator_UnitTests.fs
@@ -0,0 +1,34 @@
+﻿namespace PodFul.WPF.UnitTests
+
+open System
+open FsUnit
+open NUnit.Framework
+open PodFul.WPF
+
+type TextTruncator_UnitTests() =
+
+    [<Test>]
+    member public this.``Value is not of string type so empty string is returned.``() =
+        let converter = new TextTruncator(17)
+        converter.Convert(new Object(), null, null, null) |> should equal String.Empty;
+
+    [<Test>]
+    member public this.``Text length is below maximum character limit so full text is returned.``() =
+        let converter = new TextTruncator(17)
+        converter.Convert("The red fox", null, null, null) |> should equal "The red fox";
+
+    [<Test>]
+    member public this.``Text length is at maximum character limit so full text is returned.``() =
+        let converter = new TextTruncator(17)
+        converter.Convert("The quick red fox", null, null, null) |> should equal "The quick red fox";
+
+    [<Test>]
+    member public this.``Text length is above maximum character limit so truncated text is returned.``() =
+        let converter = new TextTruncator(17)
+        converter.Convert("The quick red fox jumped over the lazy brown dog", null, null, null) |> should equal "The quick red ...";
+
+    [<Test>]
+    member public this.``Word is split by maximum character limit so is not included in truncated text.``() =
+        let converter = new TextTruncator(16)
+        converter.Convert("The quick red fox jumped over the lazy brown dog", null, null, null) |> should equal "The quick ...";
+
---
+++ PodFul.WPF/TextTruncator.cs
@@ -0,0 +1,53 @@
+﻿
+namespace PodFul.WPF
+{
+  using System;
+  using System.Globalization;
+  using System.Windows;
+  using System.Windows.Data;
+
+  [ValueConversion(typeof(String), typeof(String))]
+  public class TextTruncator : IValueConverter
+  {
+    private Int32 limit;
+
+    public TextTruncator() : this(100) { }
+
+    public TextTruncator(Int32 limit)
+    {
+      this.limit = limit;
+    }
+
+    public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+    {
+      var text = value as String;
+      if (text == null)
+      {
+        return String.Empty;
+      }
+
+      if (text.Length <= limit)
+      {
+        return text;
+      }
+
+      var words = text.Split(' ');
+      var s = "";
+      var index = 0;
+      while (s.Length + words[index].Length + 1 <= limit - "...".Length)
+      {
+        s = s + words[index] + " ";
+        index++;
+      }
+
+      s += "...";
+
+      return s;
+    }
+
+    public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+    {
+      return DependencyProperty.UnsetValue;
+    }
+  }
+}
