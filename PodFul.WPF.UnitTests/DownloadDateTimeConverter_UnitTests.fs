4a9da5bde6e8f3ad58b3403afef51ed101125af4
Benjamin Toynbee <toyners@hotmail.com>
2016-07-10 08:48:23 +1000

    Added download date converter so that DateTime.MinValue is swapped for "No download" text.

---
+++ PodFul.WPF.UnitTests/DownloadDateTimeConverter_UnitTests.fs
@@ -0,0 +1,31 @@
+﻿namespace PodFul.WPF.UnitTests
+
+open System
+open FsUnit
+open NUnit.Framework
+open PodFul.WPF
+
+type DownloadDateTimeConverter_UnitTests() = 
+
+    [<Test>]
+    member public this.``Value is null so empty string is returned.``() =
+        let converter = new DownloadDateTimeConverter()
+        converter.Convert(null, null, null, null) |> should equal String.Empty
+
+    [<Test>]
+    member public this.``Value is not of datetime type so empty string is returned.``() =
+        let converter = new DownloadDateTimeConverter()
+        converter.Convert(new Object(), null, null, null) |> should equal String.Empty
+
+    [<Test>]
+    member public this.``Value is DateTime MinValue so No download string is returned.``() =
+        let converter = new DownloadDateTimeConverter()
+        converter.Convert(DateTime.MinValue, null, null, null) |> should equal "No download"
+
+    [<Test>]
+    [<TestCase(2006, 11, 15, 23, 59, 59, "15-Nov-2006 23:59:59")>]
+    [<TestCase(2006, 11, 5, 2, 6, 7, "5-Nov-2006 2:06:07")>]
+    member public this.``Value is valid DateTime so date time string is returned.``(year, month, day, hour, minute, second, expected : string) =
+        let converter = new DownloadDateTimeConverter()
+        let value = new DateTime(year, month, day, hour, minute, second)
+        converter.Convert(value, null, null, null) |> should equal expected
---
+++ PodFul.WPF/DownloadDateTimeConverter.cs
@@ -0,0 +1,34 @@
+﻿
+namespace PodFul.WPF
+{
+  using System;
+  using System.Globalization;
+  using System.Windows;
+  using System.Windows.Data;
+
+  [ValueConversion(typeof(DateTime), typeof(String))]
+  public class DownloadDateTimeConverter : IValueConverter
+  {
+    public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+    {
+      if (value == null || !(value is DateTime))
+      {
+        return String.Empty;
+      }
+
+      var dateTime = (DateTime)value;
+
+      if (dateTime == DateTime.MinValue)
+      {
+        return "No download";
+      }
+
+      return dateTime.ToString("d-MMM-yyyy H:mm:ss");
+    }
+
+    public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+    {
+      return DependencyProperty.UnsetValue;
+    }
+  }
+}
