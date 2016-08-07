f337ba146a6739339669a6d3b7395079ca7148e2
toyners <toyners@hotmail.com>
2016-08-08 08:09:53 +1000

    Added initial version of podcast properties window.

---
+++ PodFul.WPF/PodcastPropertiesWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace PodFul.WPF
+{
+  using System;
+  using System.Windows;
+  using PodFul.Library;
+
+  /// <summary>
+  /// Interaction logic for PodcastPropertiesWindow.xaml
+  /// </summary>
+  public partial class PodcastPropertiesWindow : Window
+  {
+    public PodcastPropertiesWindow(Podcast podcast)
+    {
+      InitializeComponent();
+      this.DataContext = podcast; 
+    }
+
+    private void CloseButton_Click(Object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
