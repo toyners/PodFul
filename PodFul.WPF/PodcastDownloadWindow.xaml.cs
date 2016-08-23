cc3161595e08bc7a9b6cb549a5c3ed9288f71e78
toyners <toyners@hotmail.com>
2016-08-24 07:59:48 +1000

    Added PodcastDownloadWindow skeleton.

---
+++ PodFul.WPF/PodcastDownloadWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PodFul.WPF
+{
+  /// <summary>
+  /// Interaction logic for PodcastDownloadWindow.xaml
+  /// </summary>
+  public partial class PodcastDownloadWindow : Window
+  {
+    public PodcastDownloadWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void CancelAll_Click(Object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void FeedList_MouseWheel(Object sender, MouseWheelEventArgs e)
+    {
+    }
+  }
+}
