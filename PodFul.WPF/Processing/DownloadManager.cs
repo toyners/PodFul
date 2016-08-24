02d9dad65a6eb7fbf5712cc0bf8e141df541e97f
toyners <toyners@hotmail.com>
2016-08-25 08:05:03 +1000

    Added DownloadManager skeleton class

---
+++ PodFul.WPF/Processing/DownloadManager.cs
@@ -0,0 +1,30 @@
+﻿
+namespace PodFul.WPF.Processing
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  using Library;
+
+  public class DownloadManager
+  {
+    public void Download(String directory, Podcast[] podcasts, Queue<Int32> podcastsIndexes, Int32 threadCount)
+    {
+      while (podcastsIndexes.Count > 0)
+      {
+        var podcastIndex = podcastsIndexes.Dequeue();
+        var podcast = podcasts[podcastIndex];
+
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+        }
+      }
+    }
+  }
+}
