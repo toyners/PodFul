a1199b82074493fa98667816e42db25ff8913d4f
Benjamin Toynbee <toyners@hotmail.com>
2016-08-28 08:22:44 +1000

    Refactoring only: Split up file download into two helper functions

--- PodFul.Library/FileDownloader.fs
+++ PodFul.Library/FileDownloader.fs
@@ -7,33 +7,39 @@ open System.Threading
 
 type FileDownloader() =
 
+    let getResponseFromURL url : WebResponse = 
+        // Downcast the web request object to a HttpWebRequest object so that 
+        // the UserAgent property can be set.
+        let uri = Uri(url)
+
+        match (uri.Scheme) with
+        | "http" -> 
+            let request = WebRequest.Create(Uri(url)) :?> HttpWebRequest
+            request.UserAgent <- "Podful Podcatcher";
+            request.GetResponse()
+        | _ ->
+            WebRequest.Create(uri).GetResponse()
+
+    let writeToFile filePath (cancelToken: CancellationToken) (updateProgressFn: Action<int>) (response : WebResponse) =
+        
+        use stream = response.GetResponseStream()
+        use writer = new FileStream(filePath, FileMode.Create, FileAccess.Write)
+        let buffer = Array.zeroCreate 8192
+        let rec copyTo () = 
+            let read = stream.Read(buffer, 0, buffer.Length)
+            if read > 0 && not (cancelToken.IsCancellationRequested) then
+                writer.Write(buffer, 0, read)
+                if updateProgressFn <> null then
+                    updateProgressFn.Invoke read |> ignore
+                copyTo ()
+
+        copyTo ()
+
     let download (url: string) filePath (cancelToken: CancellationToken) (updateProgressFn: Action<int>) =
         try
-            // Downcast the web request object to a HttpWebRequest object so that 
-            // the UserAgent property can be set.
-            let uri = Uri(url)
-            let response = 
-                match (uri.Scheme) with
-                | "http" -> 
-                    let request = WebRequest.Create(Uri(url)) :?> HttpWebRequest
-                    request.UserAgent <- "Podful Podcatcher";
-                    request.GetResponse()
-                | _ ->
-                    WebRequest.Create(uri).GetResponse()
-
-            use stream = response.GetResponseStream()
-            use writer = new FileStream(filePath, FileMode.Create, FileAccess.Write)
-
-            let buffer = Array.zeroCreate 8192
-            let rec copyTo () = 
-                let read = stream.Read(buffer, 0, buffer.Length)
-                if read > 0 && not (cancelToken.IsCancellationRequested) then
-                    writer.Write(buffer, 0, read)
-                    if updateProgressFn <> null then
-                        updateProgressFn.Invoke read |> ignore
-                    copyTo ()
-
-            copyTo ()
+            getResponseFromURL url |> 
+            writeToFile filePath cancelToken updateProgressFn
+            
         with
         | :? System.Net.WebException as webex ->
                 match (webex.Response = null) with
