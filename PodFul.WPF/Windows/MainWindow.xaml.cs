cc7f16eec6be11e45d9fef53823b1d57ec4fbef6
toyners <toyners@hotmail.com>
2016-09-14 07:19:26 +1000

    Resized cancel button for podcast download. Removed unneeded code

--- PodFul.WPF/Windows/MainWindow.xaml.cs
+++ PodFul.WPF/Windows/MainWindow.xaml.cs
@@ -173,31 +173,6 @@ namespace PodFul.WPF
       this.FeedList.SelectedIndex = index;
     }
 
-    /*private Int32 SynchroniseWithExistingFiles(Feed feed)
-    {
-      var syncCount = 0;
-      for (int i = 0; i < feed.Podcasts.Length; i++)
-      {
-        var podcastUpdated = false;
-        var podcast = feed.Podcasts[i];
-        var podcastFilePath = Path.Combine(feed.Directory, podcast.FileName);
-        var podcastFileInfo = new FileInfo(podcastFilePath);
-        if (!podcastFileInfo.Exists)
-        {
-          continue;
-        }
-
-        podcast.SetFileDetails(podcastFileInfo.Length, podcastFileInfo.CreationTime);
-
-        if (podcastUpdated)
-        {
-          syncCount++;
-        }
-      }
-
-      return syncCount;
-    }*/
-
     private void Settings_Click(Object sender, RoutedEventArgs e)
     {
       var settingsWindow = new SettingsWindow(this.settings);
