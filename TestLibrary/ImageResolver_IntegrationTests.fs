196024e7d6d79dd68758b85a6f380b3a98ea16b4
toyners <toyners@hotmail.com>
2016-09-20 07:31:42 +1000

    Expanded on the image resolver test

--- TestLibrary/ImageResolver_IntegrationTests.fs
+++ TestLibrary/ImageResolver_IntegrationTests.fs
@@ -7,7 +7,7 @@ open Jabberwocky.Toolkit.IO
 open NUnit.Framework
 open PodFul.Library
 
-type Test_IntegrationTests() =
+type ImageResolver_IntegrationTests() =
 
     let workingDirectory = @"C:\Projects\PodFul\PodFul.Library.IntegrationTests\ImageResolver_IntergrationTests\";
 
@@ -21,6 +21,9 @@ type Test_IntegrationTests() =
         // Arrange
         let defaultImagePath = @"C:\DefaultImage.jpg"
         let imageDirectory = workingDirectory + @"ImageDirectory\"
+
+        DirectoryOperations.EnsureDirectoryIsEmpty(imageDirectory)
+
         let fileName = "Image.jpg"
         let urlPath = workingDirectory + fileName
         let localPath = ""
@@ -28,13 +31,32 @@ type Test_IntegrationTests() =
         let renameFunction = fun n -> if n = urlPath then fileName else failwith "Incorrect Parameters"
         let imageResolver = ImageResolver(imageDirectory, defaultImagePath, false, renameFunction) :> IImageResolver
 
-        //let assembly = Assembly.GetExecutingAssembly()
-        //assembly.CopyEmbeddedResourceToFile(fileName, urlPath)
+        let assembly = Assembly.GetExecutingAssembly()
+        assembly.CopyEmbeddedResourceToFile(fileName, urlPath)
         
         // Act
         let resultPath = imageResolver.GetName2 localPath urlPath
         
         // Assert
         Assert.AreEqual(resultPath, expectedPath)
-        //resultPath |> should equal expectedPath
-        //File.Exists(expectedPath) |> should equal true
+        Assert.AreEqual(File.Exists(expectedPath), true);
+
+    (*[<Test>]
+    member public this.``Local path set and file exists so local path is returned``()=
+        ignore
+
+    [<Test>]
+    member public this.``Local path not set and URL path not set so default path is returned``()=
+        ignore
+
+    [<Test>]
+    member public this.``Local path set but file not found locally so is downloaded and local path is returned``()=
+        ignore
+
+    [<Test>]
+    member public this.``Local name set to default but file not found locally so default name is returned``()=
+        ignore
+
+    [<Test>]
+    member public this.``Local name set to default and URL name is set so file is downloaded and updated local name is returned``()=
+        ignore*)
