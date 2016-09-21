94aefc53bca57245ab7c4c1652ffb679711a775f
toyners <toyners@hotmail.com>
2016-09-22 07:04:47 +1000

    Finished refactoring GetName2.

--- PodFul.Library/ImageResolver.fs
+++ PodFul.Library/ImageResolver.fs
@@ -64,20 +64,25 @@ type ImageResolver(imageDirectoryPath : string, defaultImagePath : string, retur
 
         member this.GetName2 (localPath : string) (urlPath : string) : string = 
 
-            if (localPath = "" || localPath = defaultImagePath) && urlPath <> "" then
-                let localName = renameFunction urlPath
-                let savePath = Path.Combine(directoryPath, localName)
-                let fileDownloader = new FileDownloader()
-                fileDownloader.Download(urlPath, savePath, System.Threading.CancellationToken.None, null) |> ignore
-                savePath
-            else if (localPath = "" || localPath = defaultImagePath) && urlPath = "" then
-                defaultImagePath
-            else if localPath <> "" && File.Exists(localPath) then
-                localPath
-            else if localPath <> "" && File.Exists(localPath) = false then
-                let fileDownloader = new FileDownloader()
-                fileDownloader.Download(urlPath, localPath, System.Threading.CancellationToken.None, null) |> ignore
-                localPath
+            let gotLocalPath = String.IsNullOrEmpty(localPath) = false
+            let gotURLPath = String.IsNullOrEmpty(urlPath) = false
+
+            if (gotLocalPath = false || localPath = defaultImagePath) then
+                
+                if gotURLPath then    
+                    let localName = renameFunction urlPath
+                    let savePath = Path.Combine(directoryPath, localName)
+                    let fileDownloader = new FileDownloader()
+                    fileDownloader.Download(urlPath, savePath, System.Threading.CancellationToken.None, null) |> ignore
+                    savePath
+                else
+                    defaultImagePath
+                
+            else if gotLocalPath then
+                 if (File.Exists(localPath)) = false then
+                    let fileDownloader = new FileDownloader()
+                    fileDownloader.Download(urlPath, localPath, System.Threading.CancellationToken.None, null) |> ignore
+                 localPath    
             else
                 failwith "Not Implemented"
             
