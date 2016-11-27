cfb093db298793062eed269e1a1d5e3756cbc982
Benjamin Toynbee <toyners@hotmail.com>
2016-11-27 20:01:59 +1100

    Completed integration tests for reading/writings Settings object. Functionality completed to get all tests running green

--- PodFul.WPF.IntegrationTests/Settings_IntegrationTests.fs
+++ PodFul.WPF.IntegrationTests/Settings_IntegrationTests.fs
@@ -2,27 +2,33 @@
 
 open System
 open System.IO
+open Jabberwocky.Toolkit.IO
 open NUnit.Framework
+open PodFul.Library
 open PodFul.WPF
 open PodFul.WPF.Logging
 
-type SettingsFactory_IntegrationTests() =
+type Settings_IntegrationTests() =
 
-    let workingDirectory = @"C:\Projects\PodFul\PodFul.Library.IntegrationTests\SettingsFactory_IntegrationTests\";
+    let workingDirectory = @"C:\Projects\PodFul\PodFul.Library.IntegrationTests\Settings_IntegrationTests\";
+
+    [<SetUp>]
+    member public this.SetupBeforeEachTest() =
+        DirectoryOperations.EnsureDirectoryIsEmpty(workingDirectory)
 
     [<Test>]
     member public this.``File path is null and causes a meaningful exception to be thrown``() =
 
         let mutable testSuccessful = false
         try
-            new Settings(null, null) |> ignore
+            new Settings(null, new FileDeliveryLogger()) |> ignore
         with
         | _ as e ->
             let isNullReferenceException = e :? System.NullReferenceException
             Assert.IsTrue(isNullReferenceException)
 
             let nullReferenceException = e :?> System.NullReferenceException
-            Assert.AreEqual(nullReferenceException.Message, "Parameter 'fileDeliveryLogger' is null.")
+            Assert.AreEqual(nullReferenceException.Message, "Parameter 'filePath' is null.")
 
             testSuccessful <- true
 
@@ -33,7 +39,7 @@ type SettingsFactory_IntegrationTests() =
 
         let mutable testSuccessful = false
         try
-            new Settings(String.Empty, null) |> ignore
+            new Settings(String.Empty, new FileDeliveryLogger()) |> ignore
         with
         | _ as e ->
             let isArgumentException = e :? System.ArgumentException
@@ -47,11 +53,13 @@ type SettingsFactory_IntegrationTests() =
         Assert.IsTrue(testSuccessful)
 
     [<Test>]
-    member public this.``Filepath is illegal and causes a meaningful exception to be thrown``() =
+    [<TestCase(@"C:\BadCharacter?InFolder\FileName.txt")>]
+    [<TestCase(@"C:\BadCharacter?InFileName.txt")>]
+    member public this.``Filepath is illegal and causes a meaningful exception to be thrown``(badFilePath : string) =
 
         let mutable testSuccessful = false
         try
-            new Settings(@"C:\BadCharacter?InName", null) |> ignore
+            new Settings(badFilePath, new FileDeliveryLogger()) |> ignore
         with
         | _ as e ->
             let isArgumentException = e :? System.ArgumentException
@@ -65,11 +73,33 @@ type SettingsFactory_IntegrationTests() =
         Assert.IsTrue(testSuccessful)
 
     [<Test>]
-    member public this.``File delivery logger is null and causes a meaningful exception to be thrown``() =
+    [<TestCase(@"..\Folder\FileName.txt")>]
+    [<TestCase(@".\FileName.txt")>]
+    [<TestCase(@"FileName.txt")>]
+    member public this.``Filepath is relative and causes a meaningful exception to be thrown``(relativeFilePath : string) =
 
         let mutable testSuccessful = false
         try
-            new Settings(@"C:\test.txt", null) |> ignore
+            new Settings(relativeFilePath, new FileDeliveryLogger()) |> ignore
+        with
+        | _ as e ->
+            let isArgumentException = e :? System.ArgumentException
+            Assert.IsTrue(isArgumentException)
+
+            let argumentReferenceException = e :?> System.ArgumentException
+            Assert.AreEqual(argumentReferenceException.Message, "Parameter 'filePath' contains a relative path.")
+
+            testSuccessful <- true
+
+        Assert.IsTrue(testSuccessful)
+
+    [<Test>]
+    member public this.``File delivery logger is null and causes a meaningful exception to be thrown``() =
+
+        let mutable testSuccessful = false
+        let filePath = workingDirectory + @"File.xml"
+        try
+            new Settings(filePath, null) |> ignore
         with
         | _ as e ->
             let isNullReferenceException = e :? System.NullReferenceException
@@ -84,7 +114,7 @@ type SettingsFactory_IntegrationTests() =
 
     [<Test>]
     member public this.``File does not exist so default properties are set on settings object``() =
-        let nonExistantFilePath = @"C:\File\Does\Not\Exist.xml"
+        let nonExistantFilePath = workingDirectory + @"FileDoesNotExist.settings"
         let fileDeliveryLogger = new FileDeliveryLogger()
         let settings = new Settings(nonExistantFilePath, fileDeliveryLogger)
 
@@ -94,28 +124,36 @@ type SettingsFactory_IntegrationTests() =
 
     [<Test>]
     member public this.``Saving settings results in file being created``() =
+        
+        // Arrange
         let fileDeliveryLogger = new FileDeliveryLogger()
-        let filePath = workingDirectory + @"file.xml"
+        let filePath = workingDirectory + @"Settings.settings"
+        let fileExistsBeforeSave = File.Exists(filePath)
         let settings = new Settings(filePath, fileDeliveryLogger)
-
-        Assert.IsFalse(File.Exists(filePath))
-
+        
+        // Act
         settings.Save()
 
+        // Assert
+        Assert.IsFalse(fileExistsBeforeSave)
         Assert.IsTrue(File.Exists(filePath))
 
     [<Test>]
-    member public this.``Full cycle``() =
+    member public this.``Created settings object from file has same values as original settings object``() =
+        
+        // Arrange
         let fileDeliveryLogger = new FileDeliveryLogger()
-        let filePath = workingDirectory + @"file"
-        let settingsA = SettingsFactory.Create(filePath, fileDeliveryLogger)
+        let filePath = workingDirectory + @"Settings.settings"
+        let settingsA = new Settings(filePath, fileDeliveryLogger)
 
-        SettingsFactory.Save(filePath, settingsA)
+        settingsA.Save()
 
-        let settingsB = SettingsFactory.Create(filePath, fileDeliveryLogger)
+        // Act
+        let settingsB = new Settings(filePath, fileDeliveryLogger)
 
+        // Assert
         Assert.AreNotSame(settingsA, settingsB)
         Assert.AreEqual(settingsA.ConcurrentDownloadCount, settingsB.ConcurrentDownloadCount)
         Assert.AreEqual(settingsA.ConfirmPodcastDownloadThreshold, settingsB.ConfirmPodcastDownloadThreshold)
-        //Assert.(settingsA.ConfirmPodcastDownloadThreshold, settingsB.ConfirmPodcastDownloadThreshold)
+        Assert.AreEqual(settingsA.DeliveryPoints.Length, settingsB.DeliveryPoints.Length)
         
--- PodFul.WPF/Settings.cs
+++ PodFul.WPF/Settings.cs
@@ -2,16 +2,22 @@
 namespace PodFul.WPF
 {
   using System;
+  using System.Collections.Generic;
   using System.IO;
   using System.Runtime.Serialization;
   using System.Xml;
   using System.Xml.Schema;
   using System.Xml.Serialization;
+  using Jabberwocky.Toolkit.Object;
   //using Newtonsoft.Json;
   using PodFul.Library;
 
   public class Settings
   {
+    private String filePath;
+
+    private SettingsData settingsData;
+
     #region Construction
     public static Settings Create(String filePath, ILogger fileDeliveryLogger)
     {
@@ -34,20 +40,72 @@ namespace PodFul.WPF
       throw new NotImplementedException();
     }
 
-    public Settings(String filePath, ILogger log)
+    public Settings(String filePath, ILogger fileDeliveryLogger)
     {
-      this.ConcurrentDownloadCount = 3;
+      fileDeliveryLogger.VerifyThatObjectIsNotNull("Parameter 'fileDeliveryLogger' is null.");
+      filePath.VerifyThatObjectIsNotNull("Parameter 'filePath' is null.");
+      if (filePath == String.Empty)
+      {
+        throw new ArgumentException("Parameter 'filePath' is empty.");
+      }
 
-      this.ConfirmPodcastDownloadThreshold = 3;
+      if (!Path.IsPathRooted(filePath))
+      {
+        throw new ArgumentException("Parameter 'filePath' contains a relative path.");
+      }
 
-      this.DeliveryPoints = this.CreateDeliveryPoints(log);
+      this.filePath = filePath;
+
+      if (File.Exists(this.filePath))
+      {
+        this.LoadFromFile(fileDeliveryLogger);
+      }
+      else
+      {
+        try
+        {
+          var file = File.Create(this.filePath);
+          file.Close();
+        }
+        catch (ArgumentException a)
+        {
+          if (a.Message == "Illegal characters in path.")
+          {
+            throw new ArgumentException("Parameter 'filePath' contains characters that are illegal in file paths.");
+          }
+        }
+
+        this.settingsData = new SettingsData
+        {
+          ConcurrentDownloadCount = 3,
+          ConfirmPodcastDownloadThreshold = 3,
+          DeliveryData = new[]
+          {
+            new SettingsData.DeliveryPointData
+            {
+              Name = "Winamp",
+              Location = @"C:\Program Files (x86)\Winamp\winamp.exe"
+            }
+          }
+        };
+      }
+
+      this.DeliveryPoints = this.CreateDeliveryPoints(this.settingsData.DeliveryData, fileDeliveryLogger);
     }
     #endregion
 
     #region Properties
-    public Int32 ConfirmPodcastDownloadThreshold { get; set; }
+    public Int32 ConfirmPodcastDownloadThreshold
+    {
+      get { return this.settingsData.ConfirmPodcastDownloadThreshold; }
+      set { this.settingsData.ConfirmPodcastDownloadThreshold = value; }
+    }
 
-    public UInt32 ConcurrentDownloadCount { get; set; }
+    public UInt32 ConcurrentDownloadCount
+    {
+      get { return this.settingsData.ConcurrentDownloadCount; }
+      set { this.settingsData.ConcurrentDownloadCount = value; }
+    }
 
     public Action<Podcast, String>[] DeliveryPoints { get; private set; }
     #endregion
@@ -56,15 +114,62 @@ namespace PodFul.WPF
 
     public void Save()
     {
-
+      var serializer = new XmlSerializer(typeof(SettingsData));
+      using (var fileStream = new FileStream(this.filePath, FileMode.Create))
+      {
+        serializer.Serialize(fileStream, this.settingsData);
+      }
     }
 
-    private Action<Podcast, String>[] CreateDeliveryPoints(ILogger log)
+    private Action<Podcast, String>[] CreateDeliveryPoints(SettingsData.DeliveryPointData[] deliveryPointData, ILogger log)
     {
-      return new Action<Podcast, String>[]
+      Action<Podcast, String>[] deliveryPoints = new Action<Podcast, String>[deliveryPointData.Length];
+      var index = 0;
+
+      foreach (var data in deliveryPointData)
       {
-        new WinampDeliveryPoint(@"C:\Program Files (x86)\Winamp\winamp.exe", log.Message, log.Exception).DeliverToWinamp
-      };
+        switch (data.Name)
+        {
+          case "Winamp": deliveryPoints[index] = new WinampDeliveryPoint(data.Location, log.Message, log.Exception).DeliverToWinamp;
+          break;
+          default: throw new NotImplementedException("Delivery point '" + data.Name + "' not recognised");
+        }
+
+        index++;
+      }
+
+      return deliveryPoints;
+    }
+
+    private void LoadFromFile(ILogger fileDeliveryLogger)
+    {
+      XmlSerializer serializer = new XmlSerializer(typeof(SettingsData));
+      using (var fileStream = new FileStream(this.filePath, FileMode.Open))
+      {
+        this.settingsData = (SettingsData)serializer.Deserialize(fileStream);
+      }
+
+      this.settingsData.DeliveryData = new SettingsData.DeliveryPointData[1];
+      this.settingsData.DeliveryData[0].Name = "Winamp";
+      this.settingsData.DeliveryData[0].Location = @"C:\Program Files (x86)\Winamp\winamp.exe";
+    }
+    #endregion
+
+    #region Structures
+    public struct SettingsData
+    {
+      public Int32 ConfirmPodcastDownloadThreshold;
+
+      public UInt32 ConcurrentDownloadCount;
+
+      public DeliveryPointData[] DeliveryData;
+
+      public struct DeliveryPointData
+      {
+        public String Name;
+
+        public String Location;
+      }
     }
     #endregion
   }
