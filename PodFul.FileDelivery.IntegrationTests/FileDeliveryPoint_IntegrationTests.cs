26adc528e1eea3e25948e03c381daccb7f9ba5da
Benjamin Toynbee <toyners@hotmail.com>
2016-12-10 20:53:29 +1100

    Completed integration test for FileDeliveryPoint

--- PodFul.FileDelivery.IntegrationTests/FileDeliveryPoint_IntegrationTests.cs
+++ PodFul.FileDelivery.IntegrationTests/FileDeliveryPoint_IntegrationTests.cs
@@ -17,20 +17,37 @@ namespace PodFul.FileDelivery.IntegrationTests
     [SetUp]
     public void SetupBeforeEachTest()
     {
+      this.workingDirectory = Path.Combine(Path.GetTempPath(), "FileDeliveryPoint_IntegrationTests");
       DirectoryOperations.EnsureDirectoryIsEmpty(this.workingDirectory);
     }
 
     [Test]
     public void Test()
     {
-      var filePath = Path.Combine(workingDirectory, Path.GetTempFileName());
-      var destinationPath = Path.Combine(workingDirectory, "Destination");
-      var fileDeliveryPoint = new FileDeliveryPoint(destinationPath, null, null);
+      // Arrange
+      var fileName = Path.GetRandomFileName();
+      var filePath = Path.Combine(workingDirectory, fileName);
+      var fileStream = File.Create(filePath);
+      fileStream.WriteByte(1);
+      fileStream.Close();
 
+      var destinationPath = Path.Combine(workingDirectory);
+      var expectedDestinationFilePath = Path.Combine(destinationPath, DateTime.Now.ToString("dd-mm-yyyy") + "_1");
+      expectedDestinationFilePath = Path.Combine(expectedDestinationFilePath, fileName);
+
+      var fileDeliveryPoint = new FileDeliveryPoint(destinationPath, this.PostMessage, this.PostException);
+
+      // Act
+      fileDeliveryPoint.Initialise();
       fileDeliveryPoint.Deliver(filePath, "file");
 
-
+      // Assert
+      File.Exists(expectedDestinationFilePath).ShouldBeTrue();
     }
+
+    private void PostMessage(String message) { }
+
+    private void PostException(String message) { }
     #endregion 
   }
 }
