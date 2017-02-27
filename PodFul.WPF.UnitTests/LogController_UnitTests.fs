d7d38cb527541be16a38af1a9f178302b07891f3
toyners <toyners@hotmail.com>
2017-02-28 07:08:05 +1100

    Completed unit tests for LogController

--- PodFul.WPF.UnitTests/LogController_UnitTests.fs
+++ PodFul.WPF.UnitTests/LogController_UnitTests.fs
@@ -2,25 +2,63 @@
 
 open System
 open System.Collections.Generic
-open FsUnit
 open NUnit.Framework
 open PodFul.WPF.Logging
 
 type LogController_UnitTests() = 
     
     [<Test>]
-    member public this.``test``() =
+    member public this.``Passing a null reference to the cstr throws meaningful exception``() =
         
         let mutable testPassed = false
         try
             let controller = new LogController(null);
             testPassed <- false
         with
-            | ex -> 
-                let message = ex.Message
-                if message = "" then
-                    testPassed <- true  
+            | e -> 
+                Assert.AreEqual("Parameter 'loggers' is null.", e.Message)
+                testPassed <- true  
 
-        testPassed |> should equal true
+        Assert.AreEqual(true, testPassed)
 
+    [<Test>]
+    member public this.``Passing a null reference to a logger to the cstr throws meaningful exception``() =
+        
+        let mutable testPassed = false
+        try
+            let controller = new LogController(null);
+            testPassed <- false
+        with
+            | e -> 
+                Assert.AreEqual("Parameter 'loggers' is null.", e.Message)
+                testPassed <- true  
 
+        Assert.AreEqual(true, testPassed)
+
+    [<Test>]
+    member public this.``Passing non-matching key when displaying message throws a meaningful exception``() =
+        
+        let mutable testPassed = false
+        try
+            let controller = new LogController(null);
+            testPassed <- false
+        with
+            | e -> 
+                Assert.AreEqual("", e.Message)
+                testPassed <- true
+
+        Assert.AreEqual(true, testPassed)
+
+    [<Test>]
+    member public this.``Passing non-matching key when getting logger throws a meaningful exception``() =
+        
+        let mutable testPassed = false
+        try
+            let controller = new LogController(null);
+            testPassed <- false
+        with
+            | e -> 
+                Assert.AreEqual("", e.Message)
+                testPassed <- true
+
+        Assert.AreEqual(true, testPassed)
