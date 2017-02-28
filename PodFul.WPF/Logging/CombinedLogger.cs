8b0f852514fc2fb1544b98e7ff21a8a53ab1f287
toyners <toyners@hotmail.com>
2017-03-01 06:44:07 +1100

    DEV 64: Refactoring only: Moved CombinedLogger class to it's own file

---
+++ PodFul.WPF/Logging/CombinedLogger.cs
@@ -0,0 +1,33 @@
+﻿
+namespace PodFul.WPF.Logging
+{
+  using System;
+  using Jabberwocky.Toolkit.Object;
+
+  public class CombinedLogger : ILogger
+  {
+    #region Members
+    private readonly FileLogger fileLogger;
+
+    private readonly GUILogger uiLogger;
+    #endregion
+
+    #region Construction
+    public CombinedLogger(FileLogger fileLogger, GUILogger uiLogger)
+    {
+      fileLogger.VerifyThatObjectIsNotNull("Parameter 'fileLogger' is null.");
+      uiLogger.VerifyThatObjectIsNotNull("Parameter 'uiLogger' is null.");
+      this.fileLogger = fileLogger;
+      this.uiLogger = uiLogger;
+    }
+    #endregion
+
+    #region Methods
+    public void Message(String message)
+    {
+      this.fileLogger.Message(message);
+      this.uiLogger.Message(message);
+    }
+    #endregion
+  }
+}
--- PodFul.WPF/Logging/GUILogger.cs
+++ PodFul.WPF/Logging/GUILogger.cs
@@ -49,31 +49,4 @@ namespace PodFul.WPF.Logging
     }
     #endregion
   }
-
-  public class CombinedLogger : ILogger
-  {
-    #region Members
-    private readonly FileLogger fileLogger;
-
-    private readonly GUILogger uiLogger;
-    #endregion
-
-    #region Construction
-    public CombinedLogger(FileLogger fileLogger, GUILogger uiLogger)
-    {
-      fileLogger.VerifyThatObjectIsNotNull("Parameter 'fileLogger' is null.");
-      uiLogger.VerifyThatObjectIsNotNull("Parameter 'uiLogger' is null.");
-      this.fileLogger = fileLogger;
-      this.uiLogger = uiLogger;
-    }
-    #endregion
-
-    #region Methods
-    public void Message(String message)
-    {
-      this.fileLogger.Message(message);
-      this.uiLogger.Message(message);
-    }
-    #endregion
-  }
 }
