3683d8b22f05b52f714b0f49ff48931026b2303a
toyners <toyners@hotmail.com>
2017-03-14 07:19:14 +1100

    DEV 73: Added UI functionality to feed properties so that the feed directory can be changed.

--- PodFul.WPF/Windows/FeedPropertiesWindow.xaml.cs
+++ PodFul.WPF/Windows/FeedPropertiesWindow.xaml.cs
@@ -3,6 +3,7 @@ namespace PodFul.WPF.Windows
 {
   using System;
   using System.Windows;
+  using System.Windows.Forms;
   using PodFul.Library;
 
   /// <summary>
@@ -10,16 +11,35 @@ namespace PodFul.WPF.Windows
   /// </summary>
   public partial class FeedPropertiesWindow : Window
   {
+    private Feed feed;
+
+    #region Construction
     public FeedPropertiesWindow(Feed feed)
     {
       InitializeComponent();
       this.Title = feed.Title;
       this.DataContext = feed;
+      this.feed = feed;
     }
+    #endregion
 
-    private void CloseButton_Click(Object sender, RoutedEventArgs e)
+    #region Methods
+    private void CloseButtonClick(Object sender, RoutedEventArgs e)
     {
       this.Close();
     }
+
+    private void ChangeDirectoryClick(Object sender, RoutedEventArgs e)
+    {
+      var folderBrowserDialog = new FolderBrowserDialog();
+      if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.Cancel)
+      {
+        return;
+      }
+
+      this.DirectoryPath.Text = folderBrowserDialog.SelectedPath;
+      //this.feed.Directory = folderBrowserDialog.SelectedPath;
+    }
+    #endregion
   }
 }
