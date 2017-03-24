5aa4852ca20b45a573d99a5dd700fb387826d7b3
Benjamin Toynbee <toyners@hotmail.com>
2017-03-25 09:27:43 +1100

    Completed functionality so that the old feed files can be read.

--- PodFul.Library/JSONFeedIO.fs
+++ PodFul.Library/JSONFeedIO.fs
@@ -6,6 +6,9 @@ open System.Text
 
 module JSONFeedIO =
 
+  let canResolveDifferences (exceptionMessage : string) : bool =
+    exceptionMessage.Contains("The data contract type 'PodFul.Library.Feed' cannot be deserialized because the required data members 'CompleteDownloadsOnScan@, DeliverDownloadsOnScan@, DoScan@' were not found.")
+    
   let ResolveFeedDifferences (previousFeed : PreviousFeed) : Feed = 
     {
         Title = previousFeed.Title
@@ -27,9 +30,17 @@ module JSONFeedIO =
     use reader = new StreamReader(filePath) 
     use ms = new MemoryStream(ASCIIEncoding.Default.GetBytes(reader.ReadToEnd())) 
 
-    (new DataContractJsonSerializer(typeof<Feed>)).ReadObject(ms) :?> Feed
-    //    let obj = (new DataContractJsonSerializer(typeof<OldFeed>)).ReadObject(ms) 
-      //  obj :?> OldFeed |> ResolveFeedDifferences
+    try
+        (new DataContractJsonSerializer(typeof<Feed>)).ReadObject(ms) :?> Feed
+    with
+    | :? System.Runtime.Serialization.SerializationException as e ->
+        if not (canResolveDifferences e.Message) then
+            reraise()
+
+        // Reset to the start of the data
+        ms.Position <- 0L
+        let obj = (new DataContractJsonSerializer(typeof<PreviousFeed>)).ReadObject(ms)
+        obj :?> PreviousFeed |> ResolveFeedDifferences
 
   let WriteFeedToFile (feed : Feed) (filePath : string) : unit = 
     use ms = new MemoryStream()
