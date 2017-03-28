1c16549434da48d286feca8a609f42ccfbcc2f1d
toyners <toyners@hotmail.com>
2017-03-28 22:27:09 +1100

    Refactoring only: Moved PreviousFeed to it's own file

--- PodFul.Library/Feed.fs
+++ PodFul.Library/Feed.fs
@@ -2,20 +2,6 @@
 
 open System
 
-type PreviousFeed = 
-    {
-        Title : string;
-        Description : string;
-        Website : string;
-        Directory : string;
-        URL : string;
-        ImageURL : string;
-        ImageFileName : string;
-        Podcasts : Podcast[];
-        CreationDateTime : DateTime
-        UpdatedDateTime : DateTime
-    }
-
 [<CustomEquality; CustomComparison>]
 type Feed = 
     {
---
+++ PodFul.Library/PreviousFeed.fs
@@ -0,0 +1,17 @@
+﻿namespace PodFul.Library
+
+open System
+
+type PreviousFeed = 
+    {
+        Title : string;
+        Description : string;
+        Website : string;
+        Directory : string;
+        URL : string;
+        ImageURL : string;
+        ImageFileName : string;
+        Podcasts : Podcast[];
+        CreationDateTime : DateTime
+        UpdatedDateTime : DateTime
+    }
