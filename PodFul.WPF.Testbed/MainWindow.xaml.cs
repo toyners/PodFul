0e657aa1059b82cd91ecb00f902ab9da347b09f0
Benjamin Toynbee <toyners@hotmail.com>
2018-02-10 20:34:15 +1100

    Added helper methods to create the test podcasts and feeds. Set up feeds for futher testing

--- PodFul.WPF.Testbed/MainWindow.xaml.cs
+++ PodFul.WPF.Testbed/MainWindow.xaml.cs
@@ -18,11 +18,21 @@ namespace PodFul.WPF.Testbed
 
     private void Button_Click(Object sender, RoutedEventArgs e)
     {
-      var podcastDetails = new PodcastFile(String.Empty, -1, DateTime.Now, String.Empty);
-      var podcast = new Podcast("Test", String.Empty, String.Empty, String.Empty, DateTime.Now, podcastDetails);
-      var oldFeed = new Feed("OldFeed", String.Empty, String.Empty, String.Empty, String.Empty, String.Empty, String.Empty, new[] { podcast }, DateTime.Now, DateTime.Now, true, true, true);
-      var window = new DownloadConfirmationWindow(oldFeed, oldFeed);
+      var oldFeed = this.CreateFeed("OldFeed", new[] { this.CreatePodcast("Podcast B"), this.CreatePodcast("Podcast C") });
+      var newFeed = this.CreateFeed("NewFeed", new[] { this.CreatePodcast("Podcast A"), this.CreatePodcast("Podcast C") });
+      var window = new DownloadConfirmationWindow(oldFeed, newFeed);
       window.ShowDialog();
+    }
+
+    private Podcast CreatePodcast(String title)
+    {
+      var podcastFile = new PodcastFile(String.Empty, -1, DateTime.Now, String.Empty);
+      return new Podcast(title, String.Empty, String.Empty, String.Empty, DateTime.Now, podcastFile);
+    }
+
+    private Feed CreateFeed(String title, Podcast[] podcasts)
+    {
+      return new Feed(title, String.Empty, String.Empty, String.Empty, String.Empty, String.Empty, String.Empty, podcasts, DateTime.Now, DateTime.Now, true, true, true);
     }
   }
 }
