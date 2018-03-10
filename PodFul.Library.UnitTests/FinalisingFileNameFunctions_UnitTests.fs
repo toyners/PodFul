b77846f25d79af894f37b72718a8c4930d1d95d1
Benjamin Toynbee <toyners@hotmail.com>
2018-03-10 14:57:32 +1100

    Completed name resolution tests for new algorithm

--- PodFul.Library.UnitTests/FinalisingFileNameFunctions_UnitTests.fs
+++ PodFul.Library.UnitTests/FinalisingFileNameFunctions_UnitTests.fs
@@ -66,8 +66,8 @@ type FinalisingFileNameFunctions_UnitTests() =
         let results = FinalisingFileNameFunctions.finaliseFileNames feedName podcasts
 
         Assert.AreEqual(2,  Array.length <| results)
-        Assert.AreEqual("", podcasts.[0].FileDetails.FileName)
-        Assert.AreEqual("", podcasts.[1].FileDetails.FileName)
+        Assert.AreEqual(String.Empty, podcasts.[0].FileDetails.FileName)
+        Assert.AreEqual(String.Empty, podcasts.[1].FileDetails.FileName)
 
     [<Test>]
     [<TestCase(null)>]
@@ -94,7 +94,7 @@ type FinalisingFileNameFunctions_UnitTests() =
     [<TestCase("http://abc.com/fileName.mp3")>]
     [<TestCase("http://abc.com/fileName.mp3?dest-id=92518")>]
     [<TestCase("http://abc.mp3.com/fileName")>]
-    member public this.``Finalising different url formats using standard finalising function``(url : string) =
+    member public this.``Finalising different url examples using standard finalising function``(url : string) =
 
         let expectedFileName = "fileName.mp3"
         let podcasts =
@@ -190,25 +190,100 @@ type FinalisingFileNameFunctions_UnitTests() =
         Assert.AreEqual(expectedFileName3, podcasts.[2].FileDetails.FileName)
 
     [<Test>]
-    member public this.``Finalising urls with clashes in standard and alternate algorithms``() =
-
-        let expectedFileName1 = "feed name episode 1.mp3"
-        let expectedFileName2 = "feed name episode 2.mp3"
-        let expectedFileName3 = "feed name episode 3.mp3"
-        let expectedFileName4 = "feed name episode 4.mp3"
+    member public this.``Alternate naming algorithm has most successful naming resolutions``() =
+        
+        let expectedFileName1 = "episode1.mp3"
+        let expectedFileName2 = "episode2.mp3"
+        let expectedFileName3 = "episode3.mp3"
+        let expectedFileName4 = feedName + " - title4.mp3"
+        let expectedFileName5 = feedName + " - title5.mp3"
+        let expectedFileName6 = feedName + " - title6.mp3"
+        let expectedFileName7 = String.Empty
 
         let podcasts =
             [|
                 Setup.createTestPodcast "title1" "description1" "http://abc.com/episode1/fileName.mp3" FeedFunctions.NoDateTime 1L FeedFunctions.NoDateTime "image1" "imageURL1" String.Empty
                 Setup.createTestPodcast "title2" "description2" "http://abc.com/episode2/fileName.mp3" FeedFunctions.NoDateTime 2L FeedFunctions.NoDateTime "image2" "imageURL2" String.Empty
-                Setup.createTestPodcast "title3" "description3" "http://abc.com/episode/fileName.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL3" String.Empty
-                Setup.createTestPodcast "title4" "description4" "http://abc.com/episode/fileName.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title3" "description3" "http://abc.com/episode3/fileName.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL3" String.Empty
+                Setup.createTestPodcast "title4" "description4" "http://abc.com/episode/fileName1.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title5" "description4" "http://abc.com/episode/fileName2.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title6" "description4" String.Empty FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title6" "description4" String.Empty FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
             |]
-            
+
         let results = FinalisingFileNameFunctions.finaliseFileNames feedName podcasts
 
-        Assert.AreEqual(4,  Array.length results)
-        Assert.AreEqual(expectedFileName4, podcasts.[0].FileDetails.FileName)
-        Assert.AreEqual(expectedFileName3, podcasts.[1].FileDetails.FileName)
-        Assert.AreEqual(expectedFileName2, podcasts.[2].FileDetails.FileName)
-        Assert.AreEqual(expectedFileName1, podcasts.[3].FileDetails.FileName)
+        Assert.AreEqual(7,  Array.length results)
+        Assert.AreEqual(expectedFileName1, podcasts.[0].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName2, podcasts.[1].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName3, podcasts.[2].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName4, podcasts.[3].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName5, podcasts.[4].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName6, podcasts.[5].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName7, podcasts.[6].FileDetails.FileName)
+
+    [<Test>]
+    member public this.``Standard naming algorithm has most successful naming resolutions``() =
+        
+        let expectedFileName1 = feedName + " - title1.mp3"
+        let expectedFileName2 = feedName + " - title2.mp3"
+        let expectedFileName3 = "fileName1.mp3"
+        let expectedFileName4 = "fileName2.mp3"
+        let expectedFileName5 = "fileName3.mp3"
+        let expectedFileName6 = feedName + " - title6.mp3"
+        let expectedFileName7 = String.Empty
+
+        let podcasts =
+            [|
+                Setup.createTestPodcast "title1" "description1" "http://abc.com/episode1/fileName.mp3" FeedFunctions.NoDateTime 1L FeedFunctions.NoDateTime "image1" "imageURL1" String.Empty
+                Setup.createTestPodcast "title2" "description2" "http://abc.com/episode2/fileName.mp3" FeedFunctions.NoDateTime 2L FeedFunctions.NoDateTime "image2" "imageURL2" String.Empty
+                Setup.createTestPodcast "title3" "description3" "http://abc.com/episode/fileName1.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL3" String.Empty
+                Setup.createTestPodcast "title4" "description4" "http://abc.com/episode/fileName2.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title5" "description4" "http://abc.com/episode/fileName3.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title6" "description4" String.Empty FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title6" "description4" String.Empty FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+            |]
+
+        let results = FinalisingFileNameFunctions.finaliseFileNames feedName podcasts
+
+        Assert.AreEqual(7,  Array.length results)
+        Assert.AreEqual(expectedFileName1, podcasts.[0].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName2, podcasts.[1].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName3, podcasts.[2].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName4, podcasts.[3].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName5, podcasts.[4].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName6, podcasts.[5].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName7, podcasts.[6].FileDetails.FileName)
+
+    [<Test>]
+    member public this.``Standard naming algorithm has the same number of resolutions as alternative naming algorithm``() =
+        
+        let expectedFileName1 = feedName + " - title1.mp3"
+        let expectedFileName2 = feedName + " - title2.mp3"
+        let expectedFileName3 = feedName + " - title3.mp3"
+        let expectedFileName4 = "fileName1.mp3"
+        let expectedFileName5 = "fileName2.mp3"
+        let expectedFileName6 = "fileName3.mp3"
+        let expectedFileName7 = feedName + " - title7.mp3"
+
+        let podcasts =
+            [|
+                Setup.createTestPodcast "title1" "description1" "http://abc.com/episode1/fileName.mp3" FeedFunctions.NoDateTime 1L FeedFunctions.NoDateTime "image1" "imageURL1" String.Empty
+                Setup.createTestPodcast "title2" "description2" "http://abc.com/episode2/fileName.mp3" FeedFunctions.NoDateTime 2L FeedFunctions.NoDateTime "image2" "imageURL2" String.Empty
+                Setup.createTestPodcast "title3" "description3" "http://abc.com/episode3/fileName.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL3" String.Empty
+                Setup.createTestPodcast "title4" "description4" "http://abc.com/episode/fileName1.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title5" "description5" "http://abc.com/episode/fileName2.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title6" "description6" "http://abc.com/episode/fileName3.mp3" FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+                Setup.createTestPodcast "title7" "description7" String.Empty FeedFunctions.NoDateTime 3L FeedFunctions.NoDateTime "image3" "imageURL4" String.Empty
+            |]
+
+        let results = FinalisingFileNameFunctions.finaliseFileNames feedName podcasts
+
+        Assert.AreEqual(7,  Array.length results)
+        Assert.AreEqual(expectedFileName1, podcasts.[0].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName2, podcasts.[1].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName3, podcasts.[2].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName4, podcasts.[3].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName5, podcasts.[4].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName6, podcasts.[5].FileDetails.FileName)
+        Assert.AreEqual(expectedFileName7, podcasts.[6].FileDetails.FileName)
