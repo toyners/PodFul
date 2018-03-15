00d162c3e8d85294ba949b3d4e96140f45d6a030
Benjamin Toynbee <toyners@hotmail.com>
2018-03-15 19:19:40 +1100

    DEV 80: Added comments to IDownloadManager

--- PodFul.WPF/Processing/IDownloadManager.cs
+++ PodFul.WPF/Processing/IDownloadManager.cs
@@ -8,14 +8,29 @@ namespace PodFul.WPF.Processing
   /// </summary>
   public interface IDownloadManager : ISimpleDownloadManager
   {
+    /// <summary>
+    /// Event raised when all jobs have finished (completed, failed or canceled).
+    /// </summary>
     event Action AllJobsFinishedEvent;
 
+    /// <summary>
+    /// Event raised when job finished successfully.
+    /// </summary>
     event Action<DownloadJob> JobCompletedSuccessfullyEvent;
 
+    /// <summary>
+    /// Event raised when job has finished (completed, failed or canceled).
+    /// </summary>
     event Action<DownloadJob> JobFinishedEvent;
 
+    /// <summary>
+    /// Event raised when job moves from waiting to running.
+    /// </summary>
     event Action<DownloadJob> JobStartedEvent;
 
+    /// <summary>
+    /// Event raised when job is queued on download manager (i.e. added).
+    /// </summary>
     event Action<DownloadJob> JobQueuedEvent;
   }
 }
