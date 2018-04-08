5c8f9ecdff104dd546ff6f2d579c82f00a907a38
Benjamin Toynbee <toyners@hotmail.com>
2018-04-08 10:03:20 +1000

    DEV 69: Moved TestFeedProcessor into the Testbed application

---
+++ PodFul.WPF.Testbed/TestFeedProcessor.cs
@@ -0,0 +1,45 @@
+﻿
+namespace PodFul.WPF.Testbed
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Threading;
+  using Library;
+  using Windows;
+
+  public class TestFeedProcessor : IFeedProcessor
+  {
+    public IList<Feed> Feeds { get; private set; }
+
+    public Action FinishedAddingFeed { get; set; }
+
+    public Action StartedAddingFeed { get; set; }
+
+    public Feed AddFeed(String directory, String url, String defaultPodcastImageFilePath, CancellationToken cancelToken)
+    {
+      this.StartedAddingFeed?.Invoke();
+
+      var newFeed = new Feed("Title", "Description", "", "", "", "", "",
+        new Podcast[0], DateTime.MinValue, DateTime.MinValue, true, true, true, 3u);
+
+      this.FinishedAddingFeed?.Invoke();
+
+      return newFeed;
+    }
+
+    public void RemoveFeed(Int32 index)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void RemoveFeed(Feed feed)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void ScanFeeds(IList<Int32> indexes)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
--- PodFul.WPF/Windows/MainWindowNext.xaml.cs
+++ PodFul.WPF/Windows/MainWindowNext.xaml.cs
@@ -93,42 +93,6 @@ namespace PodFul.WPF.Windows
     void ScanFeeds(IList<Int32> indexes);
   }
 
-  public class TestFeedProcessor : IFeedProcessor
-  {
-    public IList<Feed> Feeds { get; private set; }
-
-    public Action FinishedAddingFeed { get; set; }
-
-    public Action StartedAddingFeed { get; set; }
-
-    public Feed AddFeed(String directory, String url, String defaultPodcastImageFilePath, CancellationToken cancelToken)
-    {
-      this.StartedAddingFeed?.Invoke();
-
-      var newFeed = new Feed("Title", "Description", "", "", "", "", "",
-        new Podcast[0], DateTime.MinValue, DateTime.MinValue, true, true, true, 3u);
-
-      this.FinishedAddingFeed?.Invoke();
-
-      return newFeed;
-    }
-
-    public void RemoveFeed(Int32 index)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void RemoveFeed(Feed feed)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ScanFeeds(IList<Int32> indexes)
-    {
-      throw new NotImplementedException();
-    }
-  }
-
   public class FeedProcessor : IFeedProcessor
   {
     private ILogController logController;
