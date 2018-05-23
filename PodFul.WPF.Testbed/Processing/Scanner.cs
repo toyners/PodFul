1dab342935649a6bc200618116d9001fae839c72
toyners <toyners@hotmail.com>
2018-05-23 17:48:42 +1000

    DEV 69: Implemented scanning using tasks with a max task count

--- PodFul.WPF.Testbed/Processing/Scanner.cs
+++ PodFul.WPF.Testbed/Processing/Scanner.cs
@@ -2,7 +2,9 @@
 namespace PodFul.WPF.Testbed.Processing
 {
   using System;
+  using System.Collections.Concurrent;
   using System.Collections.Generic;
+  using System.Threading;
   using System.Threading.Tasks;
   using ViewModel;
 
@@ -10,15 +12,45 @@ namespace PodFul.WPF.Testbed.Processing
   {
     public Action ScanCompletedEvent { get; set; }
 
-    public void ScanFeeds(IList<FeedViewModel> feeds, IDownloadManagerFactory downloadManagerFactory)
+    public void ScanFeeds(IList<FeedViewModel> feedViewModels, IDownloadManagerFactory downloadManagerFactory)
     {
-      Task.Factory.StartNew(() =>
+      var maxTaskCount = 1;
+      var scanningTask = Task.Factory.StartNew(() =>
       {
-        foreach (var feed in feeds)
-        {
-          feed.Scan(downloadManagerFactory);
-        }
+        var feedViewModelQueue = new ConcurrentQueue<FeedViewModel>(feedViewModels);
 
+        var objectLock = new Object();
+        var scanTaskCount = 0;
+        var completedScan = false;
+        FeedViewModel feedViewModel;
+
+        while (!completedScan)
+        {
+          if (scanTaskCount < maxTaskCount && feedViewModelQueue.Count > 0 && feedViewModelQueue.TryDequeue(out feedViewModel))
+          {
+            lock (objectLock)
+            {
+              scanTaskCount++;
+            }
+
+            Task.Factory.StartNew(() =>
+            {
+              feedViewModel.Scan(downloadManagerFactory);
+              lock (objectLock)
+              {
+                scanTaskCount--;
+              }
+            });
+          }
+
+          Thread.Sleep(500);
+
+          completedScan = (scanTaskCount == 0 && feedViewModelQueue.Count == 0);
+        }
+      });
+
+      scanningTask.ContinueWith((t) =>
+      {
         this.ScanCompletedEvent?.Invoke();
       });
     }
