bb5c27c2e5d0613b7a4964542f2b94a4c9da9894
toyners <toyners@hotmail.com>
2018-06-04 18:57:05 +1000

    DEV 69: Image for podcast plugged in and window height adjusted

--- PodFul.WPF.Testbed/ViewModel/PodcastViewModel.cs
+++ PodFul.WPF.Testbed/ViewModel/PodcastViewModel.cs
@@ -16,6 +16,7 @@ namespace PodFul.WPF.Testbed.ViewModel
     public DateTime DownloadDate { get { return this.podcast.FileDetails.DownloadDate; } }
     public String FilePath { get { return this.podcast.FileDetails.FileName; } }
     public Int64 FileSize { get { return this.podcast.FileDetails.FileSize; } }
+    public String ImageFileName { get { return this.podcast.FileDetails.ImageFileName; } }
     public DateTime PublishedDate { get { return this.podcast.PubDate; } }
     public String Title { get { return this.podcast.Title; } }
     public String URL { get { return this.podcast.URL; } }
