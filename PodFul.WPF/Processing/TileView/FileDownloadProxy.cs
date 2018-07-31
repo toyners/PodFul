352a4863e1da2b93bf3816c399c5277add8b6240
Benjamin Toynbee <toyners@hotmail.com>
2018-07-31 20:26:03 +1000

    DEV 69: Completed implementation of FileDownloadProxy

--- PodFul.WPF/Processing/TileView/FileDownloadProxy.cs
+++ PodFul.WPF/Processing/TileView/FileDownloadProxy.cs
@@ -3,12 +3,14 @@ namespace PodFul.WPF.Processing.TileView
 {
   using System;
   using System.Threading;
+  using PodFul.Library;
 
   public class FileDownloadProxy : IFileDownloadProxy
   {
     public void Download(string url, string destination, CancellationToken cancelToken, Action<int> downloadProgressEventHandler)
     {
-      throw new NotImplementedException();
+      var fileDownloader = new FileDownloader();
+      fileDownloader.Download(url, destination, cancelToken, downloadProgressEventHandler);
     }
   }
 }
